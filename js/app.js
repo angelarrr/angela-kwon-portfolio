be9f79f2d1bdd4d104e840fd338b27da643bab76
angelarrr <kwon.angela@gmail.com>
2015-06-17 21:43:01 -0400

    make interactive

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.masthead').mouseenter(function() {
+		$('.masthead-intro').effect("bounce", "slow");
+	});
+
+	$('.profile').hover(function() {
+		$(this).attr('src','http://i.imgur.com/14CNy1g.jpg');
+	}, function() {
+		$(this).attr('src', 'http://i.imgur.com/zy8YrAs.jpg');
+	});
+
+});
